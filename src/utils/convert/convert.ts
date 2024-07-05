4e672589748841a2033849be79f5ebb716a57bb9
Kevin <briand-kevin@hotmail.fr>
2024-07-05 09:11:28 +0200

    fix: Conversion of dates to UTC

--- src/utils/convert/convert.ts
+++ src/utils/convert/convert.ts
@@ -4,13 +4,13 @@ export const remainingTime = (nextChange: number, lang: string): string => {
   if (nextChange === -1) {
     return localize('card.never', lang)
   }
-  const date = new Date((nextChange + new Date().getTimezoneOffset() * 60) * 1000)
+  const date = new Date(nextChange * 1000)
   date.setMilliseconds(0)
-  if (date.getDate() > 1 && date.getDate() < 8) {
-    return `${date.getDate() - 1}${localize('card.dayLetter', lang)} ${date.getHours()}h`
+  if (date.getUTCDate() > 1 && date.getUTCDate() < 8) {
+    return `${date.getUTCDate() - 1}${localize('card.dayLetter', lang)} ${date.getUTCHours()}h`
   }
-  if (date.getHours() > 0 && date.getDate() === 1) {
-    return `${date.getHours()}h ${date.getMinutes()}m`
+  if (date.getUTCHours() > 0 && date.getUTCDate() === 1) {
+    return `${date.getUTCHours()}h ${date.getUTCMinutes()}m`
   }
-  return `${date.getMinutes()}m  ${date.getSeconds()}s`
+  return `${date.getUTCMinutes()}m  ${date.getUTCSeconds()}s`
 }
